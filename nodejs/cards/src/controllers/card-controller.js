3ba15a7517c48ce062be9e4bf29c036836f9c7bb
Rafael Souza <rafaelsnt123@gmail.com>
2021-01-23 14:58:22 -0300

    Edicao do codigo API em NodeJS

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -1,7 +1,50 @@
+const { request, response } = require('express')
 const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
+    neDB.find({}).sort({ name: 1}).exec((exception, cards) => {
+        if(exception) {
+            const setence = 'Could not list the cards!'
+            console.log(setence, exception)
+            response.status(exception)
+            response.json({ 'mensagem': setence })
+        }
+        response.json(cards)
+    })
 }
+
+api.save = (request, response) => {
+    const  canonical= request.body
+    neDB.insert(canonical, (exception, card) => {
+        if(exception) {
+            const setence = "Could not list the cards!"
+            console.log(setence, exception)
+            response.status(exception.status | 400)
+            response.json({ 'mensagem': setence })
+        }
+
+        response.status(201)
+        response.json(card)
+    })
+}
+
+api.delete = (request, response) => {
+    card.findByIdAndDelete(request.params.id)
+    .then((card) => {
+        if(!card) {
+            return response.status(404).send({ message: `Card Not Found ${request.params.id}`})
+        }
+
+        response.status(200).send({ message: `Card deleted successfully `, card})
+    }).catch((err) => {
+        if (err.kind === 'ObjectId') {
+            return response.status(404).send({ message: `Card not found ${request.params.id}`})
+        }
+        
+        return response.status(500).send({ message: `Error ${request.params.id}`})
+    })
+}
+
 
 module.exports = api
